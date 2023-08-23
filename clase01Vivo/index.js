9e9ace8e4900b76582903c13254f412be5a0ccff
marianellagl <marianella.gomezluna@gmail.com>
2023-08-23 18:30:00 -0300

    clase 01

---
+++ clase01Vivo/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
+  const body = document.querySelector("body");
+
+  button.addEventListener("click", function () {
+    alert("esto es un componente");
+
+    if (body.style.backgroundColor === "blue") {
+      body.style.backgroundColor = "red";
+    } else {
+      body.style.backgroundColor = "blue";
+    }
+  });
+});
